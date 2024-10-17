1fc5fe4bdecbba6b656b61ce97e0f5485ee96588
juex <juex@qq.com>
2024-10-17 10:54:35 +0800

    version 0.0.2

---
+++ SpringBootMangaDownLoader/src/main/java/my/file/springbootmangadownloader/SpringBootMangaDownLoaderApplication.java
@@ -0,0 +1,13 @@
+package my.file.springbootmangadownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMangaDownLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMangaDownLoaderApplication.class, args);
+    }
+
+}
---
+++ SpringBootMangaDownLoader/src/test/java/my/file/springbootmangadownloader/SpringBootMangaDownLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package my.file.springbootmangadownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMangaDownLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
